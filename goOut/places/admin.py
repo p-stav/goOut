41515beaabecf411b711f1e9deb132ad095cfda8
p-stav <paulstavropoulos@gmail.com>
2013-12-20 18:03:02 -0800

    updated models
    UserProfile

--- goOut/places/admin.py
+++ goOut/places/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from places.models import 	Place, UserAction, FavoritePlace, PlaceName,Hashtag
+from places.models import 	Place, UserAction, FavoritePlace, PlaceName,Hashtag, UserProfile
 
 # Register your models here.
 admin.site.register(Place)
 admin.site.register(UserAction)
+admin.site.register(UserProfile)
 admin.site.register(FavoritePlace)
 admin.site.register(PlaceName)
 admin.site.register(Hashtag)
--- goOut/places/models.py
+++ goOut/places/models.py
@@ -2,33 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Place(models.Model):
-	placeId = models.CharField(max_length = 100)
-	tag = models.CharField(max_length = 50)
-	freq = models.IntegerField()
-	time = models.DateTimeField()
-	score = models.FloatField()
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	points = models.IntegerField()
 	
 	def __unicode__(self):
-		return self.placeId
-
-class UserAction (models.Model):
-	uid = models.ForeignKey(User)
-	placeId = models.CharField(max_length=100)
-	tag = models.CharField(max_length = 50)
-	time = models.DateTimeField()
+		return self.user.username
 	
-	def __unicode__(self):
-		return self.uid.username
-		
-class FavoritePlace(models.Model):
-	uid = models.ForeignKey(User)
-	placeId = models.CharField(max_length = 100)
-	
-	def  __unicode__(self):
-		return self.placeId
-		
-
 class PlaceName(models.Model):
 	placeId = models.CharField(max_length = 100)
 	placeName = models.CharField(max_length = 50)
@@ -41,3 +21,31 @@ class Hashtag(models.Model):
 	
 	def __unicode__(self):
 		return self.tag
+		
+class Place(models.Model):
+	placeId = models.ForeignKey(PlaceName)
+	tag = models.ForeignKey(Hashtag)
+	freq = models.IntegerField()
+	time = models.DateTimeField()
+	score = models.FloatField()
+	
+	def __unicode__(self):
+		return self.placeId
+
+class UserAction (models.Model):
+	uid = models.ForeignKey(UserProfile)
+	placeId = models.ForeignKey(PlaceName)
+	tag = models.ForeignKey(Hashtag)
+	time = models.DateTimeField()
+	
+	def __unicode__(self):
+		return self.uid.user.username
+		
+class FavoritePlace(models.Model):
+	uid = models.ForeignKey(UserProfile)
+	placeId = models.ForeignKey(PlaceName)
+	
+	def  __unicode__(self):
+		return self.placeId
+		
+
