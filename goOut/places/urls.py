ff88407991ba2be09d7ce6dc68456ded2140cc33
p-stav <paulstavropoulos@gmail.com>
2013-12-24 11:25:33 -0800

    problem with google api and DJango
    Need to fix; accessing two domains at the same time FAHHHK

--- goOut/places/urls.py
+++ goOut/places/urls.py
@@ -2,24 +2,22 @@ from django.conf.urls import patterns,  url
 
 urlpatterns = patterns('places.views',
 	
-	#list of places and map
+	#list of places, search page, and map
 	url(r'^$', 'index', name='index'),
+	url(r'^search/(?P<search_term>.+/$', 'search', name='search'),
 	url(r'^/map/$', 'index', name='index'),
 	
 	
 	url(r'^submit$', 'submitReview', name='submitReview'),
 	url(r'^submit/submit$', 'submit_submitReview', name='submit_submitReview'),
 	
+	#create user's own page, view other people's profile, and see user's favorited places
 	url(r'^user/$', 'view_profile', {'username' : ''}),
 	url(r'^user/(?P<username>.+)/$', 'view_profile', name='profile'),
+	url(r'^user/(?P<username>.+)/fav/$', 'fav', name='fav'),
 	
 	#detail page about a place
-	url(r'^(?P<place_id>\d+)/$', 'placeDetail', name='detail'),
-	
-	#create list of favorited places to provide more information
-	url(r'^(?P<joke_id>\d+)/fav/$', 'fav', name='fav'),
-	
-	
+	url(r'^(?P<place_id>\d+)/$', 'placeDetail', name='detail'),	
 	
 	#url(r'^user/fav$', 'view_fav', name='user_fav'),
 	#url(r'^feedback$', 'feedback', name='feedback'),
--- goOut/places/views.py
+++ goOut/places/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from urllib import urlopen
+import json, pprint
+
+#pip install googlemaps
+import googlemaps
 
 # home/index page. 
 def index(request):
+	#find curLong + curLat
+	
+	
+	#grab json object from google Places API
+	#req = 
+	
+	
 	context = { }
 	return render(request, 'places/index.html', context)
