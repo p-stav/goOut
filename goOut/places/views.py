c2fad57e5c8b0c41561bfd1a97eba4c42fe43137
p-stav <paulstavropoulos@gmail.com>
2013-12-24 14:15:11 -0800

    got places API working with hardcoded location
    how do we do dynamic location in python?

--- goOut/places/views.py
+++ goOut/places/views.py
@@ -8,11 +8,15 @@ import googlemaps
 # home/index page. 
 def index(request):
 	#find curLong + curLat
+	#HOW DO YOU DO THIS? Hard code for now
+	curLoc = '37.798542,-122.422345'
 	
+	apiCall = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+curLoc+"&radius=550&types=bar|casino|night_club&sensor=true&key=AIzaSyAWf1WnMo_4s35yeXZ-kZyF-QZ7m5MwqP0";
 	
 	#grab json object from google Places API
-	#req = 
+	req = urlopen(apiCall).read()
+	places = json.loads(req).get("results")
 	
+	context = { 'places':places}
 	
-	context = { }
 	return render(request, 'places/index.html', context)
