8046fd0b35774af64886536e1627de0a99749745
p-stav <paulstavropoulos@gmail.com>
2013-12-26 14:26:08 -0800

    started grabbing data from database in views.py
    hi

--- goOut/places/views.py
+++ goOut/places/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from urllib import urlopen
 import json, pprint
 from datetime import datetime
-
-#pip install googlemaps
+from places.models import UserProfile, PlaceName, Hashtag, Place, UserAction, FavoritePlace
 #import googlemaps
 
 # home/index page. 
@@ -11,16 +10,15 @@ def index(request):
 	#find curLong + curLat
 	#HOW DO YOU DO THIS? Hard code for now
 	curLoc = '37.798542,-122.422345'
-	
-	
-		
+			
 	##find today's date to find items close to it in db
 	#today = datetime.today()
-	
 	#for testing purposes, hardcode datetime
-	today = datetime(2013, 12, 27, 22, 9, 41, 879000)
+	date = datetime(2013, 12, 28, 22, 40, 41, 879000)
 	
-	
+	#grab array of reviews from our models
+	cutoffTime = datetime(date.year,date.month,date.day, date.hour-2, date.minute, date.second, date.microsecond)
+	curRev = Place.objects.filter(time__gte = cutoffTime)
 	
 	apiCall = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+curLoc+"&radius=550&types=bar|casino|night_club&sensor=true&key=AIzaSyAWf1WnMo_4s35yeXZ-kZyF-QZ7m5MwqP0";
 	
