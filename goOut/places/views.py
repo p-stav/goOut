993ab619ff879d9dd5f13ff39cb667523999cdcb
p-stav <paulstavropoulos@gmail.com>
2013-12-30 13:00:28 -0800

    reference, not id
    hi

--- goOut/places/views.py
+++ goOut/places/views.py
@@ -36,38 +36,38 @@ def index(request):
 	
 	for place in places:
 		temp = {}
-		if place['id'] in curRevList:
+		if place['reference'] in curRevList:
 			#iterate over curRev and find all instances to append to dict to append to json
 			hashtags = {}
 			for i in curRev:
-				if i.placeId.placeId == place['id']:
+				if i.placeId.placeId == place['reference']:
 					hashtags[i.tag.tag] = {'score' : i.score}
 					
 			#TO-DO: create order of list
 					
 			#try/except statements because json objects don't always have 'ratings' and 'price_level'. Append to lists. 
 			#how do you use .exists() function?
-			try: temp = {'name': place['name'],'id': place['id'], 'rating':place['rating'], 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
+			try: temp = {'name': place['name'],'reference': place['reference'], 'rating':place['rating'], 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
 			except: 
-				try:temp = {'name': place['name'], 'id': place['id'], 'rating':'NA', 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
+				try:temp = {'name': place['name'], 'reference': place['reference'], 'rating':'NA', 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
 				except:
-					try: temp = {'name': place['name'], 'id': place['id'], 'rating':place['rating'], 'price_level':-1, 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
-					except: temp = {'name': place['name'], 'id': place['id'], 'rating':'NA', 'price_level':-1 , 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
+					try: temp = {'name': place['name'], 'reference': place['reference'], 'rating':place['rating'], 'price_level':-1, 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
+					except: temp = {'name': place['name'], 'reference': place['reference'], 'rating':'NA', 'price_level':-1 , 'types':place['types'], 'vicinity':place['vicinity'], 'hashtags':hashtags}
 			#append
 			placeMatch.append(temp)
 		
 		else:
 			#nested try and excepts because json objects don't always have 'ratings' and 'price_level'
-			try: temp = {'name': place['name'], 'id': place['id'], 'rating':place['rating'], 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity']}
+			try: temp = {'name': place['name'], 'reference': place['reference'], 'rating':place['rating'], 'price_level':place['price_level'], 'types':place['types'], 'vicinity':place['vicinity']}
 			except:
-				try: temp = {'name': place['name'], 'id': place['id'], 'price_level':place['price_level'], 'rating':'NA','types':place['types'], 'vicinity':place['vicinity']}
+				try: temp = {'name': place['name'], 'reference': place['reference'], 'price_level':place['price_level'], 'rating':'NA','types':place['types'], 'vicinity':place['vicinity']}
 				except:
-					try: temp = {'name': place['name'],'id': place['id'], 'price_level':-1, 'rating':place['rating'], 'types':place['types'], 'vicinity':place['vicinity']}
-					except: temp = {'name': place['name'], 'id': place['id'], 'price_level':-1, 'rating':'NA', 'types':place['types'], 'vicinity':place['vicinity']}
+					try: temp = {'name': place['name'],'reference': place['reference'], 'price_level':-1, 'rating':place['rating'], 'types':place['types'], 'vicinity':place['vicinity']}
+					except: temp = {'name': place['name'], 'reference': place['reference'], 'price_level':-1, 'rating':'NA', 'types':place['types'], 'vicinity':place['vicinity']}
 			#append
 			placeNoMatch.append(temp)
-	#examplePlace1 = {'name' : 'FBI Blacksite', 'id' : 'abcdef', 'types' : ['da club', 'blacksite'], 'vicinity' : 'lol cannot say', 'rating' : 3.5, 'price_level' : 2, 'hashtags' : {'BrosOnBros' : 4, 'TheyAreTorturingMe' : 10}}
-	#examplePlace2 = {'name' : 'Hot Mamas', 'id' : 'aghijk', 'types' : ['nomnom'], 'vicinity' : 'your mother\'s address', 'rating' : 11, 'price_level' : 0, 'hashtags' : {'BrosOnBros' : 1000000}}
+	#examplePlace1 = {'name' : 'FBI Blacksite', 'reference' : 'abcdef', 'types' : ['da club', 'blacksite'], 'vicinity' : 'lol cannot say', 'rating' : 3.5, 'price_level' : 2, 'hashtags' : {'BrosOnBros' : 4, 'TheyAreTorturingMe' : 10}}
+	#examplePlace2 = {'name' : 'Hot Mamas', 'reference' : 'aghijk', 'types' : ['nomnom'], 'vicinity' : 'your mother\'s address', 'rating' : 11, 'price_level' : 0, 'hashtags' : {'BrosOnBros' : 1000000}}
 	#examplePlaces = [examplePlace1, examplePlace2]
 
 	context = { 'placeMatch':placeMatch, 'placeNoMatch':placeNoMatch} #'examplePlaces':examplePlaces}
@@ -75,6 +75,7 @@ def index(request):
 	
 	
 def placeDetail(request,place_id):
-
+	#make call to Google API for information
+	
 	context = {}
 	return render(request, 'places/placeDetail.html', context)
