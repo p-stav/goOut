8406d9a0351696c53abf712df9f91af1c591140b
p-stav <paulstavropoulos@gmail.com>
2015-07-13 19:02:13 -0700

    word wrap

--- SimulatorView.js
+++ SimulatorView.js
@@ -1,16 +1,4 @@
-var width = Math.max(940, innerWidth),
-	height = Math.max(500, innerHeight);
-			
-var i = 0;
-var inSimulatorView = true;
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
 /*	    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
 function particle() {
@@ -33,33 +21,3 @@ function particle() {
 }*/
 
 
-
-function addUserCircle(username) {
-	svg.insert("circle", "rect")
-	.attr("cx", width / 2.0 + (Math.random() - 0.5) * width / 1.1)
-	.attr("cy", height / 2.0 + (Math.random() - 0.5) * height / 1.1)
-	.attr("r", 10)
-	.attr("id", "user_" + username);
-}
-
-function addSpreadLine(sender, receiver, hexColor) {
-	var senderCircle = $("#user_" + sender);
-	var receiverCircle = $("#user_" + receiver);
-
-	
-	svg.insert("line", "rect")
-	.attr("x1", senderCircle.attr("cx"))
-	.attr("y1", senderCircle.attr("cy"))
-	.attr("x2", senderCircle.attr("cx"))
-	.attr("y2", senderCircle.attr("cy"))
-	.attr("stroke-width", 2)
-	.attr("stroke", hexColor)
-  .transition()
-  .duration(2000)
-  .attr("x2", receiverCircle.attr("cx"))
-	.attr("y2", receiverCircle.attr("cy"))
-  .transition()
-  .duration(10000)
-  .attr("stroke-width", 0.5)
-  .style("stroke-opacity", 0.5);
-}
---
+++ simulatorSetup.js
@@ -0,0 +1,85 @@
+function djb2(str){
+  var hash = 5381;
+  for (var i = str.length - 1; i >= 0; i--) {
+    hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
+  }
+  return hash;
+}
+
+function hashRippleToColor(ripple) {
+  var hash = djb2(ripple.text);
+  var r = (hash & 0xFF0000) >> 16;
+  var g = (hash & 0x00FF00) >> 8;
+  var b = hash & 0x0000FF;
+  return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
+}
+
+function hashRippleToColor2(ripple) {
+  var hash = djb2(ripple.text);
+  var r = (hash & 0x000000) >> 16;
+  var g = (hash & 0x000000) >> 8;
+  var b = hash & 0x000000;
+  return "#fff";
+}
+
+var startInterval;
+var spreadInterval;
+var startCounter = 0;
+var spreadCounter = 0;
+
+function startRipple() {
+	if (startCounter == 1) {
+		clearInterval(startInterval);
+		spreadInterval = setInterval(spreadRipple, 400);
+		return;
+	}
+	// pick random user to start the ripple
+	var userIndex = Math.floor(Math.random() * users.length);
+	var user = users[userIndex];
+	var ripple = user.startRipple("text_" + startCounter.toString());
+	for (var j = 0; j < ripple.receivers.length; j++)
+	{
+		if (ripple.receivers[j] != user)
+			 addSpreadLine(user.name, ripple.receivers[j].name, hashRippleToColor(ripple));
+	}
+	startCounter++;
+}
+
+
+var currentRippleInSimulator;
+function isUserPending(user) {
+	if ((currentRippleInSimulator.spread.indexOf(user) == -1) && (currentRippleInSimulator.dismissed.indexOf(user) == -1)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function spreadRipple() {
+	if (spreadCounter == 10) {
+
+		clearInterval(spreadInterval);
+		restartSimulation();
+	}
+
+	var rippleIndex = Math.floor(Math.random() * ripples.length);
+	var ripple = ripples[rippleIndex];
+	
+	// attempt to spread
+	currentRippleInSimulator = ripple;
+	var pendingUsers = ripple.receivers.slice().filter(isUserPending);
+	var userIndex = Math.floor(Math.random() * pendingUsers.length);
+	var user = pendingUsers[userIndex];
+	
+	var newReceivers = user.spreadRipple(ripple);
+	for (var j = 0; j < newReceivers.length; j++) {
+		addSpreadLine(user.name, newReceivers[j].name, hashRippleToColor(ripple));
+			
+	}
+	 
+	spreadCounter++;
+}
+
+
+startInterval = setInterval(startRipple, 500);
